741f6f4487acddcee5e91a2baa6653159bab03b6
CarriDe <carrillodanielr@gmail.com>
2025-11-01 18:48:09 +0100

    Creando carpetas, php y array

---
+++ ejercicio1-php/pagos.php
@@ -0,0 +1,23 @@
+<?php
+
+$socios = [
+    "ID1" => [
+        "ID" => 1, "Nombre" => "Sabrina", "Apellidos" => "Spellman", "DNI" => "98765432Q",
+        "Mail" => "salem@witch.com", "Móvil" => 666222666,
+        "pagos" => [
+            "1692-01" => ["mes" => "Enero", "Importe" => 666, "estado" => "Pagado", "Fecha de pago" => "2028-01-16"],
+            "1692-02" => ["mes" => "Febrero", "Importe" => 1, "estado" => "Pendiente", "Fecha de pago" => null],
+            ]
+        ],
+    "ID2" => [
+        "ID" => 2,
+        "Nombre" => "Harvey", "Apellidos" => "Kinkle", "DNI" => "12345678Z", "Mail" => "harvey@greendale.com", "Móvil" => 666999333,
+        "pagos" => [
+            "1692-01" => ["mes" => "Enero", "Importe" => 666, "estado" => "Pagado", "Fecha de pago" => "2028-01-16"],
+            "1692-02" => ["mes" => "Febrero", "Importe" => 1, "estado" => "Pendiente", "Fecha de pago" => null],
+            ]
+        ]
+]
+
+
+?>
